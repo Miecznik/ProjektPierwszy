4f9c81ecb0ff9518ff9a7a9a0b39f0ddc6791f33
Miecznik <hudytoja@gmail.com>
2024-04-07 13:45:55 +0200

    Update niedziela

---
+++ src/Specjalista.java
@@ -0,0 +1,16 @@
+public class Specjalista {
+
+    protected String specjalizacja;
+
+    public Specjalista(String specjalizacja) {
+        this.specjalizacja = specjalizacja;
+    }
+
+    public String getSpecjalizacja() {
+        return specjalizacja;
+    }
+
+    public void setSpecjalizacja(String specjalizacja) {
+        this.specjalizacja = specjalizacja;
+    }
+}
